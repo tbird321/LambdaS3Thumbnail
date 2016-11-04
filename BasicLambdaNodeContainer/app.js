797ce75b160b885693bb8aef5bbf5ef4a1c9cc95
tbird321 <tbird321@DESKTOP-D2TNF5J>
2016-11-04 00:38:13 -0600

    d

--- BasicLambdaNodeContainer/app.js
+++ BasicLambdaNodeContainer/app.js
@@ -12,34 +12,34 @@ var edge = require('edge');
 //    console.log(result);
 //});
 
-var clrMethod = edge.func({
-    assemblyFile: './NonCoreDLL/BasicNonCore.dll',
-    typeName: 'BasicNonCore.Startup',
-    methodName: 'Invoke' // This must be Func<object,Task<object>>
-});
+//var clrMethod = edge.func({
+//    assemblyFile: './NonCoreDLL/BasicNonCore.dll',
+//    typeName: 'BasicNonCore.Startup',
+//    methodName: 'Invoke' // This must be Func<object,Task<object>>
+//});
 
-clrMethod(1, function (error, result) {    
-    console.log(result);
-});
+//clrMethod(1, function (error, result) {    
+//    console.log(result);
+//});
 
-//var exec = require('child_process').exec;
-//console.log('Starting App');
-//var context = {};
-//context.done = function () {
-//    console.log("Lambda Function Complete");
-//}
+var exec = require('child_process').exec;
+console.log('Starting App');
+var context = {};
+context.done = function () {
+    console.log("Lambda Function Complete");
+}
 
-//handler = function (event, context,callback) {
-//    child = exec("BasicLambda.dll", { cwd: 'CoreDLL' }, function (error,stdout,stderr) {
-//        if (stderr) {
-//            console.log("call failed");
-//        }
-//        else {
-//            console.log("Completed call");
-//        }
-//    });
-//    child.stdout.on('data', console.log);
-//    child.stderr.on('data', console.error);
-//}
+handler = function (event, context,callback) {
+    child = exec("BasicLambda.dll", { cwd: 'CoreDLL' }, function (error,stdout,stderr) {
+        if (stderr) {
+            console.log("call failed");
+        }
+        else {
+            console.log("Completed call");
+        }
+    });
+    child.stdout.on('data', console.log);
+    child.stderr.on('data', console.error);
+}
 
-//handler(null, context);
+handler(null, context);
